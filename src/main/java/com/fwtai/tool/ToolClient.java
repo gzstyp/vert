4b83ccc5fb67d4dad18d8304c0e0ad160d5b30fb
gzstyp <444141300@qq.com>
2020-08-26 23:03:03 +0800

    响应客户端

--- src/main/java/com/fwtai/tool/ToolClient.java
+++ src/main/java/com/fwtai/tool/ToolClient.java
@@ -1,13 +1,18 @@
 package com.fwtai.tool;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
 
 public final class ToolClient{
 
-  public static String json(final int code,final String msg){
+  public static String createJson(final int code,final String msg){
     final JsonObject json = new JsonObject();
     json.put("code",code);
     json.put("msg",msg);
     return json.encode();
+  }
+
+  public static void responseJson(final RoutingContext request,final String json){
+    request.response().putHeader("Cache-Control","no-cache").putHeader("content-type","application/json;charset=UTF-8").end(json);
   }
 }
