47742b7dc341e0613a24039bba5e47772ff36404
gzstyp <444141300@qq.com>
2020-10-09 23:02:25 +0800

    注释

--- src/main/java/com/fwtai/tool/ToolLambda.java
+++ src/main/java/com/fwtai/tool/ToolLambda.java
@@ -20,7 +20,7 @@ import java.util.List;
  * @QQ号码 444141300
  * @Email service@dwlai.com
  * @官网 http://www.fwtai.com
- */
+*/
 public final class ToolLambda{
 
   public static Future<JsonObject> getConfig(final ConfigRetriever retriever){
--- src/main/java/com/fwtai/tool/ToolMySQL.java
+++ src/main/java/com/fwtai/tool/ToolMySQL.java
@@ -28,7 +28,7 @@ import java.util.List;
  * @QQ号码 444141300
  * @Email service@yinlz.com
  * @官网 <url>http://www.yinlz.com</url>
- */
+*/
 public final class ToolMySQL{
 
   final InternalLogger logger = Log4JLoggerFactory.getInstance(getClass());
